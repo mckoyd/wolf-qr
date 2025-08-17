052212b02619f8035b2e7dbfeea7f61d18da9aa8
Wolf <wolf@Wolfs-MacBook-Pro.local>
2025-08-17 00:37:27 -0400

    adds repo base

--- src/components/QRCodeCard/config.ts
+++ src/components/QRCodeCard/config.ts
@@ -1,7 +1,11 @@
+const REPO_BASE = process.env.NEXT_PUBLIC_GH_REPO
+  ? `/${process.env.NEXT_PUBLIC_GH_REPO}`
+  : "";
+
 export const content = {
   title: "Improve your front-end skills by building projects",
   description:
     "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level",
   imageAlt: "QR code to visit Frontend Mentor website",
-  imageSrc: "/assets/image-qr-code.png",
+  imageSrc: `${REPO_BASE}/assets/image-qr-code.png`,
 };
