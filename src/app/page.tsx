d82f0a08e08ad943f1a8725a3fb978b41fb7ce00
Wolf <wolf@Wolfs-MacBook-Pro.local>
2025-08-17 00:22:01 -0400

    escapes apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 const Home: React.FC = () => {
   return (
     <main className={styles.container}>
-      <h1 className="sr-only">Wolf's QR Code Component</h1>
+      <h1 className="sr-only">Wolf&apos;s QR Code Component</h1>
       <QRCodeCard />
     </main>
   );
