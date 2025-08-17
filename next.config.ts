989d0f6de95b46cacdf0a81b2171657ad8e7fadc
Wolf <wolf@Wolfs-MacBook-Pro.local>
2025-08-16 23:59:18 -0400

    updates next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = process.env.NEXT_PUBLIC_GH_REPO || "wolf-qr";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}/` : undefined,
 };
 
 export default nextConfig;
